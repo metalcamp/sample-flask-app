4fda6743436623e9c1cddfb84680fd09dea8474d
Mario Kranjec <mariokranjec91@gmail.com>
2020-06-15 17:59:39 +0200

    add contact form - post request

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     text = "Yet another message from handler"
     current_year = datetime.datetime.now().year
@@ -12,7 +12,7 @@ def index():
     return render_template("index.html", text=text, current_year=current_year, cities=cities)
 
 
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET"])
 def about_me():
     return render_template("about-me.html")
 
@@ -20,6 +20,21 @@ def about_me():
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    # TODO validation
+
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
