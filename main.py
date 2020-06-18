0b12793fada6eae6d08af065ea8e58160b939435
Mario Kranjec <mariokranjec91@gmail.com>
2020-06-18 17:27:34 +0200

    wip

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template, request, make_response
 import datetime
 
@@ -6,55 +8,32 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    text = "Yet another message from handler"
-    current_year = datetime.datetime.now().year
-    cities = ["Boston", "Vienna", "Paris"]
-    return render_template("index.html", text=text, current_year=current_year, cities=cities)
+    secret_number = request.cookies.get("secret_number")
 
-
-@app.route("/about-me", methods=["GET", "POST"])
-def about_me():
-    if request.method == "GET":
-        user_name = request.cookies.get("user_name")
-        return render_template("about-me.html", name=user_name)
-
-    elif request.method == "POST":
-        # TODO validation
-
-        contact_name = request.form.get("contact-name")
-        contact_email = request.form.get("contact-email")
-        contact_message = request.form.get("contact-message")
-
-        print(contact_name)
-        print(contact_email)
-        print(contact_message)
-
-        return render_template("success.html")
-
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
-
-
-@app.route("/contact", methods=["POST"])
-def contact():
-    # TODO validation
-
-    contact_name = request.form.get("contact-name")
-    contact_email = request.form.get("contact-email")
-    contact_message = request.form.get("contact-message")
-
-    print(contact_name)
-    print(contact_email)
-    print(contact_message)
-
-    # return render_template("success.html")
-
-    response = make_response(render_template("success.html"))
-    response.set_cookie("user_name", contact_name)
+    response = make_response(render_template("index.html"))
+    if not secret_number:
+        new_secret = random.randint(1,30)
+        response.set_cookie("secret_number", str(new_secret))
 
     return response
+
+@app.route("/result", methods=["POST"])
+def get_result():
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Correct"
+        response = make_response(render_template("result.html", message=message))
+        response.set_cookie("secret_number", str(random.randint(1,30)))
+        return response
+    elif guess > secret_number:
+        message = "Your guess is not correct... try something smaller"
+        return render_template("result.html", message=message)
+    elif guess < secret_number:
+        message = "Your guess is not correct... try something bigger"
+        return render_template("result.html", message=message)
+
 
 if __name__ == '__main__':
     app.run()
